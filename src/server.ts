e90c8c41901deba63a4028e2a0ab1a9d37daa3b4
RafaDom10 <r-domingues@outlook.com>
2021-02-24 15:20:11 -0300

    added SurveyController and tests

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,3 @@
-import 'reflect-metadata';
+import { app } from "./app";
 
-import express from 'express';
-
-import './database';
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
- 
 app.listen(3333, () => console.log('> Server started at http://localhost:3333'));
